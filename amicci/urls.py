c59669f3c2f0b8fb670d646fdec0c6b7ca75ec5b
rafaellevissa <rafaellevissa@gmail.com>
2024-08-25 15:52:48 -0300

    Add briefing url

--- amicci/urls.py
+++ amicci/urls.py
@@ -33,9 +33,11 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/demo/briefings/', include('api.briefing.urls')),
     path('api/demo/retailers/', include('api.retailer.urls')),
     path('api/demo/vendors/', include('api.vendor.urls')),
     path('api/demo/categories/', include('api.category.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
