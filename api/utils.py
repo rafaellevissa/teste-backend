f5c75b0a19ea156631a2d34257c8f6e783395726
rafaellevissa <rafaellevissa@gmail.com>
2024-08-25 15:57:48 -0300

    Add exception handler

---
+++ api/utils.py
@@ -0,0 +1,14 @@
+from rest_framework.views import exception_handler
+from rest_framework.response import Response
+from rest_framework import status
+
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+
+    if response is None:
+        return Response(
+            {'error': 'server_error', 'message': str(exc)},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+    return response
