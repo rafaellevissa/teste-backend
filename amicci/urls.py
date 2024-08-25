7043944fe9e067138600efd949301cdeaefe466a
rafaellevissa <rafaellevissa@gmail.com>
2024-08-25 15:39:21 -0300

    Add vendor routes

--- amicci/urls.py
+++ amicci/urls.py
@@ -33,6 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/demo/vendors/', include('api.vendor.urls')),
     path('api/demo/categories/', include('api.category.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
